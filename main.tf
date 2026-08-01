37511efe70a795f056ef5dea9c77ea9f48f128c6
Thoyaz <thoyazthoyazpc@gmail.com>
2026-08-01 11:11:36 +0530

    create networking with tf

--- main.tf
+++ main.tf
@@ -138,20 +138,3 @@ resource "aws_route_table_association" "database" {
   route_table_id = aws_route_table.database.id
 }
 
-resource "aws_vpc_peering_connection" "default" {
-  count = var.is_peering_required ? 1 : 0
-  #peer_owner_id = var.peer_owner_id
-  peer_vpc_id   = data.aws_vpc.default.id #vpc-00d9de542572a13ec
-  vpc_id        = var.requester_vpc_id #vpc-0fd2c34702a25e94e
-  auto_accept = true
-
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  tags = local.all_vpc_peering_connection_tags
-}
---
+++ peering.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc_peering_connection" "default" {
+  count = var.is_peering_required ? 1 : 0
+  #peer_owner_id = var.peer_owner_id
+  peer_vpc_id   = data.aws_vpc.default.id #vpc-00d9de542572a13ec
+  vpc_id        = var.requester_vpc_id #vpc-0fd2c34702a25e94e
+  auto_accept = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  tags = local.all_vpc_peering_connection_tags
+}
+
+# Routes for public subnets to the peered VPC
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+}
+
+# Peered VPC route to the public subnets
+resource "aws_route" "default_vpc_to_public_vpc" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block    = aws_vpc_peering_connection.default.id
+  vpc_peering_connection_id = data.aws_vpc.default.cidr_block
+}
