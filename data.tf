bf57f6bdc12fe9cca661935c2d3c0c97eab42a16
Thoyaz <thoyazthoyazpc@gmail.com>
2026-08-01 11:12:32 +0530

    create networking with tf

--- data.tf
+++ data.tf
@@ -5,7 +5,3 @@ data "aws_availability_zones" "available" {
 data "aws_vpc" "default" {
   default = true
 }
-
-data "default_vpc_route_table" {
-  default_vpc_id = data.aws_vpc.default.main_route_table_id
-}
